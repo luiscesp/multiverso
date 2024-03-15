78d0029bf657c861f1afa58a91180b662f85be40
luiscesp <balonespi@gmail.com>
2024-03-15 07:37:20 -0500

    subiendo heroes

---
+++ Superhero.java
@@ -0,0 +1,13 @@
+package herencias;
+
+public class Superhero extends persona {
+
+	public Superhero(persona person, String heroname, String) {
+		this.name= person.name;
+		this.edad= person
+		
+	}
+	String herohame;
+	String powers[];
+
+}
---
+++ persona.java
@@ -0,0 +1,30 @@
+package herencias;
+public class persona {
+   String name;
+   int edad;
+   
+   public persona(String name, int edad) {
+	   this.name= name;
+	   this.edad=18;
+			   
+   
+   }
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public int getEdad() {
+	return edad;
+}
+
+public void setEdad(int edad) {
+	this.edad = edad;
+}
+   
+   
+}
